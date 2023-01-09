2f91391c766c4f13450b0b4d43c436555150e82d
imLIVI <lesly120898@gmail.com>
2023-01-09 20:44:29 +0300

    Implement a simple Server and Client

--- src/main/java/Client/Main.java
+++ src/main/java/Client/Main.java
@@ -1,4 +1,36 @@
 package Client;
 
+import ReaderSettingsFile.ReaderSettingFile;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Main {
+    public static int port;
+    public static String ip;
+
+    public static void main(String[] args) {
+        // Reading the port and IP from the settings file
+        ReaderSettingFile readerSettingFile = new ReaderSettingFile();
+        port = readerSettingFile.getPort();
+        ip = readerSettingFile.getIp();
+
+        try (Socket clientSocket = new Socket(ip, port)) {
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            out.println("Vika");
+
+            String response = in.readLine();
+            System.out.println(response);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/Server/Main.java
+++ src/main/java/Server/Main.java
@@ -12,9 +12,23 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Server started");
 
-        // Reading the port and IP from the settings file
+        // Reading the port from the settings file
         ReaderSettingFile readerSettingFile = new ReaderSettingFile();
         port = readerSettingFile.getPort();
-        
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                System.out.println("New connection. Port: " + clientSocket.getPort());
+
+                final String name = in.readLine();
+                out.println(String.format("Hello %s, you port is %d", name, clientSocket.getPort()));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
